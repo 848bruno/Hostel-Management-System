d655f0fa9d86b00860ab7039a9ead2252b118337
848bruno <ambalebruno848@gmail.com>
2025-05-29 12:08:58 +0300

    updated env files established connection and tested the API

--- hostel-management-system/src/app.module.ts
+++ hostel-management-system/src/app.module.ts
@@ -1,12 +1,27 @@
-import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdminModule } from './admin/admin.module';
 import { AdminProfileModule } from './adminProfile/adminProfile.module';
+import { ConfigModule } from '@nestjs/config';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { LoggerMiddleware } from './logger.middleware';
 
 @Module({
-  imports: [AdminModule, AdminProfileModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    AdminModule, AdminProfileModule,
+  ],
+ 
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+    configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes('admin', 'adminprofile',);
+  }
+}
