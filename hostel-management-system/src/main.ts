b98c06d16e3b8f5f22b6bd0d56856c8cc7548e62
848bruno <ambalebruno848@gmail.com>
2025-06-03 15:19:47 +0300

    added API documentation

--- hostel-management-system/src/main.ts
+++ hostel-management-system/src/main.ts
@@ -19,9 +19,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('Hostel API')
+  .setDescription('Hostel Management API')
+  .setVersion('1.0')
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe());
 
   const configService = app.get(ConfigService);
