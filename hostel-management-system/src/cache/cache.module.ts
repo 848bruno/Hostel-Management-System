a9ce7c8d05331bb45202d819c8f52d2ea9b964d4
848bruno <ambalebruno848@gmail.com>
2025-06-05 05:47:45 +0300

    updated cache logic

--- hostel-management-system/src/cache/cache.module.ts
+++ hostel-management-system/src/cache/cache.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
 import { CacheModule as NestCacheModule } from '@nestjs/cache-manager';
 import { CacheService } from './cache.service';
+import * as redisStore from 'cache-manager-ioredis';
+import { CacheController } from './cache.controller';
 
 @Module({
   imports: [
-    NestCacheModule.register({}),
+    NestCacheModule.registerAsync({
+      useFactory: () => ({
+        store: redisStore,
+        host: process.env.REDIS_HOST || 'redis-cache',
+        port: parseInt(process.env.REDIS_PORT || '6379'),
+        password: process.env.REDIS_PASSWORD || 'password',
+        ttl: 60, // seconds
+      }),
+    }),
   ],
+   controllers: [CacheController],
   providers: [CacheService],
-  exports: [CacheService], 
+  exports: [CacheService],
+
 })
 export class CacheModule {}
